57f23650624438bf010bb4657485492b833d534f
techner3 <58848985+techner3@users.noreply.github.com>
2022-08-28 12:43:02 +0530

    Data Ingestion Config Added

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,9 +1,28 @@
-from collections import namedtuple
-from logging import logger 
-from src.utils import read_yaml
+import os 
+from src.entity import DataIngestionConfig
+from src.utils import read_yaml, create_directory
 from src.constant import CONFIG_PATH
 class Configuration:
 
     def __init__(self):
-        
+
         self.config=read_yaml(CONFIG_PATH)
+
+    def get_dataIngestion_config(self):
+
+        try:
+            ingestion_dir=os.path.join(self.config["artifacts"]["dir"],
+                                    self.config["artifacts"]["ingestion"])
+            create_directory(ingestion_dir)
+            dataIngestion_config=DataIngestionConfig(
+                                raw_data_dir=self.config["raw_data"],
+                                target=self.config["target"],
+                                ingestion_trainData_path=os.path.join(ingestion_dir,
+                                                        self.config["artifacts"]["train_data"]),
+                                ingestion_testData_path=os.path.join(ingestion_dir,
+                                                        self.config["artifacts"]["test_data"])
+            )
+            return dataIngestion_config
+
+        except Exception as e:
+            raise e
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,1 +1,4 @@
 from collections import namedtuple
+
+DataIngestionConfig=namedtuple("DataIngestionConfig",
+                                ["raw_data_dir","target","ingestion_trainData_path","ingestion_testData_path"])
