b178eba2959c17d19480e6d734eda411086f6559
techner3 <58848985+techner3@users.noreply.github.com>
2022-08-28 12:25:02 +0530

    Constant folder Added

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,6 +1,9 @@
 from collections import namedtuple
 from logging import logger 
+from src.utils import read_yaml
+from src.constant import CONFIG_PATH
 class Configuration:
 
     def __init__(self):
-        pass
+        
+        self.config=read_yaml(CONFIG_PATH)
---
+++ src/constant/__init__.py
@@ -0,0 +1,1 @@
+CONFIG_PATH="configs/configs.yaml"
